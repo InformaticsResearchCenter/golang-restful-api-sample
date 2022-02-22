7fb4335a2048a231a44ff84c2ea98e26210a49a2
Tri Angga Dio Simamora <trianggadio@gmail.com>
2022-02-22 14:33:52 +0700

    struct request validator

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"database/sql"
+	"github.com/go-playground/validator/v10"
 	"irc/golang-restful-api-sample/helper"
 	"irc/golang-restful-api-sample/model/api"
 	"irc/golang-restful-api-sample/model/domain"
@@ -12,9 +13,13 @@ import (
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service *CategoryServiceImpl) Create(ctx context.Context, request api.CategoryCreateRequest) api.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
@@ -29,6 +34,9 @@ func (service *CategoryServiceImpl) Create(ctx context.Context, request api.Cate
 }
 
 func (service *CategoryServiceImpl) Update(ctx context.Context, request api.CategoryUpdateRequest) api.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
